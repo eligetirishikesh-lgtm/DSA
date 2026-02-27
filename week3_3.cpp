677d6059402c788238035a62e7bfb129ae86cb25
eligetirishikesh-lgtm <eligetirishikesh@gmail.com>
2026-02-27 13:20:32 +0530

    Add files via upload

---
+++ week3_3.cpp
@@ -0,0 +1,19 @@
+week3_3.cpp
+#include <iostream>
+#include <map>
+using namespace std;
+
+int main() {
+    map<int, string> students;
+
+    students[101] = "Ravi";
+    students[102] = "Anita";
+    students[103] = "Kiran";
+
+    cout << "Student Details:\n";
+    for(map<int, string>::iterator it = students.begin(); it != students.end(); it++) {
+        cout << "ID: " << it->first << " Name: " << it->second << endl;
+    }
+
+    return 0;
+}
